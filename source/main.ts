f28be3e466fb8ba4ad9675497f386e80759a4a51
Silitonix <silitonix@gmail.com>
2024-03-16 10:13:55 +0330

    replace div with root and added css

--- source/main.ts
+++ source/main.ts
@@ -1,12 +1,6 @@
 import { Canvas } from "./canvas";
 
-const root = document.querySelector(".root");
-
-if (root === null)
-{
-  throw "Root element not found!";
-}
 
 const main = new Canvas({
-  parent: root
+  parent: document.body
 });
